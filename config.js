55d44c61c7b151413532fe8790e37293b45e3a14
targee <targee@cosa.org.tw>
2019-12-25 14:53:11 +0800

    remove useless prefix

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-directive/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'RESCUE ACTON BY YOUTH',
   subHeading: '青年學生體檢政府網站計畫',
   projects: [
