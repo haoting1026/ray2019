c452c4b740bfa801d6b97f7502286e1dc32cbf4e
XanxusX <tako30215@hotmail.com>
2020-04-06 14:54:37 +0800

    Add png

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Project from '../components/Project'
+import Project from '../components/Project';
 
 const IndexPage = () => (
   <Layout>
@@ -11,11 +11,28 @@ const IndexPage = () => (
     <div id="main">
       <header className="major container medium">
         <h2>
+          前言
+        </h2>
+        <p style={{ 'textAlign': 'left', 'textTransform': 'none', }}>
+          前言的內文前言的內文前言的內文前言的內文前言的內文前言的內文前言的內文前言的內文前言的內文前言的內文前言的內文前言的內文前言的內文
+        </p>
+      </header>
+      <header className="major container medium">
+        <h2>
           計劃源起
         </h2>
-        <p style={{'textAlign': 'left', 'textTransform': 'none',}}>
+        <p style={{ 'textAlign': 'left', 'textTransform': 'none', }}>
           2019年，我們以「協助優化政府網站數位服務」為計畫目的，招募具UI（User Interface）使用者介面或UX（User Experience）使用者體驗相關專長或實務經驗的青年學生，檢核服務後提供改善後的數位服務模型，供各網站主管機關和社會大眾參考。
-        </p> 
+        </p>
+      </header>
+      <header className="major container medium">
+        <h2>
+          設計步驟
+        </h2>
+        <p style={{ 'textAlign': 'left', 'textTransform': 'none', }}>
+          2019年，我們以「協助優化政府網站數位服務」為計畫目的，招募具UI（User Interface）使用者介面或UX（User Experience）使用者體驗相關專長或實務經驗的青年學生，檢核服務後提供改善後的數位服務模型，供各網站主管機關和社會大眾參考。
+        </p>
+        <img src='https://github.com/PDIS/ray2019/blob/master/src/assets/images/Ray_process.png?raw=true' alt='' width={"100%"}></img>
       </header>
       <header className="major container medium">
         <h2>
